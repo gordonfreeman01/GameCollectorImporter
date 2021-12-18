891e48b0ae94859fe1761a2259259ead10c2db98
gordonfreeman01 <gordonfreeman01@gmail.com>
2021-12-19 03:54:18 +0400

    Direct JSON to XML file export done

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # GameCollectorImporter
 A tool to batch import entries into CLZ Game Collector
+
+## Usage
+
+### Convert JSON File to XML
+
+The json2xml utility converts any JSON to a corresponding XML file without any schema transformations applied.
+
+```bash
+python .\gcimporter.py --file <input JSON file path> --output <output XML file path> --logfile <optional log file custom path>
+```
+
+For example, in the following test.json is converted into test.xml with both files being stored one folder above the script's. This command also overrides the path for the log file to be stored one folder up (by default it is stored in the script's folder) and sets log level to debug.
+
+```bash
+python .\gcimporter.py --input ../test.json --output ../test.xml --logfile ../log.txt --loglevel debug
+```
---
+++ gcimporter.py
@@ -0,0 +1,59 @@
+import json2xml
+import argparse
+import logging
+
+#Declare argument parser
+parser = argparse.ArgumentParser(description="A utility to convert a JSON to Game Collector XML format")
+parser.add_argument("-i","--input", help="Input JSON file")
+parser.add_argument("-o","--output", help="Output JSON file")
+parser.add_argument("-lf", "--logfile", help="Log file path", default="./log.txt")
+parser.add_argument("-ll", "--loglevel", help="Set logging level. Example --loglevel debug, default=INFO'", default="info")
+
+#Parse arguments
+args = parser.parse_args()
+logFilePath = args.logfile
+logLevel = args.loglevel.upper()
+inputFile = args.input
+outputFile = args.output
+
+#Declare logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logLevel)
+
+logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+#rootLogger = logging.getLogger()
+
+fileHandler = logging.FileHandler(logFilePath)
+fileHandler.setFormatter(logFormatter)
+logger.addHandler(fileHandler)
+
+consoleHandler = logging.StreamHandler()
+consoleHandler.setFormatter(logFormatter)
+logger.addHandler(consoleHandler)
+
+#Convert JSON to XML
+logger.info("JSON import started with file: {0}.".format(inputFile))
+logger.debug("args: {0}".format(args))
+
+from json2xml import json2xml
+from json2xml.utils import readfromjson
+
+jsonData = readfromjson(inputFile)
+
+#Redirect stdout to file from https://stackabuse.com/writing-to-a-file-with-pythons-print-function/
+import sys
+# Save a reference to the original standard output
+original_stdout = sys.stdout
+
+with open(outputFile, 'w') as f:
+    # Change the standard output to the file we created.
+    sys.stdout = f
+    print(json2xml.Json2xml(jsonData).to_xml())
+    logger.info("XML output to file: {0}".format(outputFile))
+     # Reset the standard output to its original value
+    sys.stdout = original_stdout
+
+#TODO read line by line and append to file (library only converts a single json at a time)
+#TODO XSLT to GC XML format
+
+logger.info("XML export completed.")
